f0eb5d47e6c7baf83e31f9c4ef075877c8d55b2e
Ingemar Finn <ingemar.finn@king.com>
2018-06-25 22:53:56 +0200

    Added files for app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// https://babeljs.io/repl
+
+const App = function() {
+    return <div>hello.</div>
+};
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
