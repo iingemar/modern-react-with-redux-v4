c77f0786b018b5a6e7e5ef22bca6a484f1f8f116
Ingemar Finn <iingemar@gmail.com>
2018-08-31 14:35:28 +0200

    App: added video list items

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YoutubeSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 // https://babeljs.io/repl
 
@@ -31,6 +32,7 @@ class App extends React.Component {
             <div>
                 <div>hello.</div>
                 <SearchBar />
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
