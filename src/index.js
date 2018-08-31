76c96a2a41ac85b01bb7d65f94a95c0537e14373
Ingemar Finn <iingemar@gmail.com>
2018-08-31 12:53:25 +0200

    Added search bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 // https://babeljs.io/repl
 
 const YOUTUBE_API_KEY = 'AIzaSyAeY6ehp-7Ty1xMsWyIS7qXPXZR840Qc4c';
 
 const App = () => {
-    return <div>hello.</div>
+    return (
+        <div>
+            <div>hello.</div>
+            <SearchBar />
+        </div>
+    );
 };
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
