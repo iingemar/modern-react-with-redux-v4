49c312635ebd6c000de3ce455bcda5cac1f1f046
Ingemar Finn <iingemar@gmail.com>
2018-08-31 16:44:36 +0200

    Added css

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -15,7 +15,7 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div>
+            <div className='search-bar'>
                 <input
                     value={this.state.userInput}
                     onChange={this.onInputChange}/>
