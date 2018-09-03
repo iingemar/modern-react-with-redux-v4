3a147c150f3026b5de60cb6fb9cb9e6657a48efd
Ingemar Hjälmstad <ingemar.hjalmstad@king.com>
2018-09-03 13:45:30 +0200

    Added react-redux connect

--- another_app/src/containers/book-list.js
+++ another_app/src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
@@ -18,5 +19,15 @@ export default class BookList extends Component {
             </ul>
         );
     }
-
 }
+
+// Whenever application state changes, bookList will re render.
+function mapStateToProps(state) {
+    // Whatever is returned from here will show up as props inside BookList
+    return {
+        books: state.books
+    }
+}
+
+// Takes function and component. Connect produces a container that is aware of state in redux
+export default connect(mapStateToProps)(BookList);
