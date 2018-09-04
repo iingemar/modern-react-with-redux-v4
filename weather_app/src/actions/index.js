20bd677c2d48eba92fe2f461a332cdcb83ab48fb
Ingemar Finn <iingemar@gmail.com>
2018-09-04 11:21:50 +0200

    Added weather reducer

--- weather_app/src/actions/index.js
+++ weather_app/src/actions/index.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
 const API_KEY = 'd4d9f90dd1fe1274ac88596dbfbbe3f5';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
+
+    console.log('request;', request);
+    // Redux-promise middleware
+    // Does this payload contain a promise?
+    // No. Continue to reducer
+    // YES! Stop this action. After promise resolves,
+    // create a new action and send it to the reducers.
 
     return {
         type: FETCH_WEATHER,
