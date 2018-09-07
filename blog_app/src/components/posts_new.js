6a07a8528683ddf9682f241832405163ca3e248e
Ingemar Finn <iingemar@gmail.com>
2018-09-07 12:21:38 +0200

    Input fields: added form validation

--- blog_app/src/components/posts_new.js
+++ blog_app/src/components/posts_new.js
@@ -9,6 +9,7 @@ class PostsNew extends Component {
             <div className="form-group">
                 <label>{field.label}</label>
                 <input className="form-control" type="text" {...field.input} />
+                {field.meta.error}
             </div>
         );
     }
@@ -38,8 +39,30 @@ class PostsNew extends Component {
     }
 }
 
+// Called automatically on submit!
+//  values = {title: "sadf", content: "asdf", categories: "asdf"}
+function validate(values) {
+    const errors = {};
+
+    // Validate the input from 'values'
+    if (!values.title || values.title.length < 3) {
+        errors.title = "Please enter a title that is at least 3 characters!"
+    }
+    if (!values.content) {
+        errors.content = "Please enter some content!"
+    }
+    if (!values.categories) {
+        errors.categories = "Please enter a category!"
+    }
+
+    // If errors is empty, the form is fine to submit.
+    // If errors has *any* properties, redux form assumes form is invalid.
+    return errors;
+}
+
 // Like the connect helper. Make it interact with redux-form
 // The form name needs to be unique!
 export default reduxForm({
+    validate: validate,
     form: 'PostsNewForm'
 })(PostsNew);
