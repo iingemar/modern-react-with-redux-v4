4f9b56c9712127daf094f120eb5437d67d8c4a52
Ingemar Finn <iingemar@gmail.com>
2018-09-07 11:19:57 +0200

    Added input fields

--- blog_app/src/components/posts_new.js
+++ blog_app/src/components/posts_new.js
@@ -2,13 +2,44 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    // Spread operator magic! Instead of onChange={field.input.onChange} etc...
+    // Put all the props of field.input on to the input element.
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input className="form-control" type="text" {...field.input} />
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>PostsNew!</div>
+            <div>
+                <form>
+                    <Field
+                        name="title"
+                        label="Title"
+                        component={this.renderField}
+                    />
+                    <Field
+                        name="content"
+                        label="Content"
+                        component={this.renderField}
+                    />
+                    <Field
+                        name="categories"
+                        label="Categories"
+                        component={this.renderField}
+                    />
+                </form>
+            </div>
         );
     }
 }
 
+// Like the connect helper. Make it interact with redux-form
+// The form name needs to be unique!
 export default reduxForm({
     form: 'PostsNewForm'
 })(PostsNew);
